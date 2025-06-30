77742a9d70981dcfa4c37035b56c85d62ea7da58
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-30 13:57:12 -0400

    Debbie first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,11 @@ import AddTask from "./components/AddTask";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes } from "react-router";
 
+const navigate = useNavigate();
+//With the routes in place, we can declare routes, Links
+
 const App = () => {
+
   const [tasks, setTasks] = useState([]);
 
   async function fetchAllTasks() {
@@ -23,24 +27,28 @@ const App = () => {
     fetchAllTasks();
   }, []);
 
-  return (
+    return(
     <div>
       <NavBar />
-      <TaskList tasks={tasks} fetchAllTasks={fetchAllTasks} />
-      <AddTask fetchAllTasks={fetchAllTasks} />
       <Routes>
-        {/* Currently, we don't have any routes defined. And you can see above that we're
-            rendering the TaskList and AddTask components directly, no matter what our URL looks like.
-            Let's fix that! */}
+        <Route
+          path ="./TaskList" element = {<TaskList tasks={tasks} fetchAllTasks={fetchAllTasks} />} 
+        />
+        <Route
+          path ="./AddTask" element ={<AddTask fetchAllTasks={fetchAllTasks} />} 
+        />
       </Routes>
-    </div>
-  );
-};
+    </div>)
 
-// We're using React Router to handle the navigation between pages.
-// It's important that the Router is at the top level of our app,
-// and that we wrap our entire app in it. With this in place, we can
-// declare routes, Links, and use useful hooks like useNavigate.
+}
+
+{/* <>
+<Link to ={`/Addtask/${Taskid}`}>AllTasks</Link>
+<Link to ="/">text</Link>
+<Link to ="/">text</Link>
+</> */}
+
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <Router>
