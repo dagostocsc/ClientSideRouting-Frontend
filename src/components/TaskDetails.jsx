d63655b86c9816d3b6819dbfa56fd0613e4ff2e9
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-30 23:11:51 -0400

    Fixed white page when reloading specific task page and removed comments from TaskDetails

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -3,35 +3,10 @@ import axios from "axios";
 import "./TaskCardStyles.css";
 import { useParams } from "react-router";
 
-/*
-<div className={`task-card ${task.completed ? "completed" : "incomplete"}`}>
-      <div className="task-card-header">
-        <h2>{task.title}</h2>
-        <div className="task-card-header-buttons">
-          {task.completed ? (
-            <p onClick={handleCompleteTask}>🔄</p>
-          ) : (
-            <p onClick={handleCompleteTask}>✅</p>
-          )}
-          <p onClick={handleDeleteTask}>🗑️</p>
-        </div>
-      </div>
-      <p>{task.description}</p>
-    </div>
-
-    <div className="task-card-header">
-      <h2>{task.title}</h2>
-      <h2>{task.description}</h2>
-      <h3>{task.userId}</h3>
-    </div>
-*/
-
 const TaskDetail = ({ API_URL }) => {
   const [task, setTask] = useState(null);
   const { id } = useParams();
   const [user, setUser] = useState(null);
-
-  console.log("Fetching task with ID:", id);
 
   useEffect(() => {
     async function fetchTask() {
@@ -39,10 +14,12 @@ const TaskDetail = ({ API_URL }) => {
         const taskResponse = await axios.get(`${API_URL}/api/tasks/${id}`);
         setTask(taskResponse.data);
 
+        console.log("Fetching task with USER ID:", taskResponse.data.userId);
         if (taskResponse.data.userId) {
           const userResponse = await axios.get(
             `${API_URL}/api/users/${taskResponse.data.userId}`
           );
+
           setUser(userResponse.data);
         }
       } catch (error) {
@@ -61,7 +38,7 @@ const TaskDetail = ({ API_URL }) => {
         <h2>{task.title}</h2>
       </div>
       <p>Description: {task.description}</p>
-      <p>User ID: {user?.name}</p>
+      <p>User Name: {user?.name}</p>
     </div>
   );
 };
